2360bb275682ba64cea05bd5b49da95dd3e4a5c4
SokolKat <SokolskayaKat@yandex.ru>
2023-01-04 17:36:13 +0300

    init: new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = `${n}`;
+    let reversed = [...str].reduce((prev,next)=>next+prev);
+    if (reversed[reversed.length - 1] == '-') {
+        reversed = reversed.slice(0, -1);
+    }
+};
